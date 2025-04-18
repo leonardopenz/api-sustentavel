1231df6c456e80dff6038e61e62fad6fd2ce50b6
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-04-18 16:06:05 -0300

    Criado o projeto no Spring Initializr

---
+++ src/main/java/br/com/senai/sustentatbilidade/SustentatbilidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.sustentatbilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustentatbilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SustentatbilidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/sustentatbilidade/SustentatbilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.sustentatbilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentatbilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
