2521b4f207f74ec53119dcc9fcb5437d51f57c05
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-01 14:41:58 -0300

    Implementado Autenticação em memória e Restrição de acesso por perfil

--- src/main/java/br/com/senai/sustentatbilidade/configurations/security/WebSecurityConfig.java
+++ src/main/java/br/com/senai/sustentatbilidade/configurations/security/WebSecurityConfig.java
@@ -2,20 +2,51 @@ package br.com.senai.sustentatbilidade.configurations.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        // Criando usuários em memória
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(
+                User.withUsername("admin")
+                        .password("{noop}admin123") // {noop} significa que a senha está em texto plano
+                        .roles("ADMIN")
+                        .build()
+        );
+
+        manager.createUser(
+                User.withUsername("user")
+                        .password("{noop}user123")
+                        .roles("USER")
+                        .build()
+        );
+
+        return manager;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorize -> authorize
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/acoes/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/api/acoes/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic();
-        return http.build();
+                .httpBasic(Customizer.withDefaults()); // Ativa autenticação via Basic Auth
 
+        return http.build();
     }
 }
