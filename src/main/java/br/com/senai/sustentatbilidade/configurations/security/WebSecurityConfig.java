eb9564a376ffeaf00b1d26438966e95f2b739e85
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-05-04 19:53:41 -0300

    Preparado o ambiente para JWT

--- src/main/java/br/com/senai/sustentatbilidade/configurations/security/WebSecurityConfig.java
+++ src/main/java/br/com/senai/sustentatbilidade/configurations/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -26,6 +27,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/acoes/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/api/acoes/**").hasRole("ADMIN")
